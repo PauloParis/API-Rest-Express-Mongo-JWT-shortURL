c7ce32f4a1a66f936a4e4994157c92e5233696d6
PauloParis <paul1963.1969@gmail.com>
2023-01-31 16:14:07 -0300

    cookies

--- middlewares/requireRefreshToken.js
+++ middlewares/requireRefreshToken.js
@@ -3,7 +3,8 @@ import { generateToken, tokenVerificationErrors } from "../utils/tokenManager.js
 
 export const requireRefreshToken = (req, res, next) => {
     try {
-        const refreshTokenCockie = req.cookies.refreshToken
+        console.log("req.cookies.refresh: ", req.cookies.refreshToken)
+        const refreshTokenCockie = req.cookies.refreshToken;
 
         if(!refreshTokenCockie) {
             throw new Error ('No existe el token r')
